bc08379d47c5e918bc8e92688dcc6fefc00c70d0
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-07-14 23:25:15 +0700

    Lấy ra danh sách bài viết

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,16 @@
+import Article from "./model/article.model";
+
 export const resolvers = {
     Query: {
       hello: () => {
         return "xin chào";
       },
+      getListArticle: async ()=>{
+        const articles = await Article.find({
+            deleted: false,
+        });
+
+        return articles;
+      }
     },
   };
