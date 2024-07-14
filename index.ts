00744db259bae362e7b7ccaf25944070968bd1dd
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-07-14 22:21:08 +0700

    Cài đặt GraphQL

--- index.ts
+++ index.ts
@@ -1,27 +1,47 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
-import Article from "./model/article.model";
+import {  ApolloServer, gql } from "apollo-server-express";
+import { typeDefs } from "./typeDefs"
+import { resolvers } from "./resolvers"
+ 
+const startSever = async () => {
+  dotenv.config();
 
-dotenv.config();
+  database.connect();
 
-database.connect();
+  const app: Express = express();
+  const port: number | String = process.env.PORT || 3000;
 
-const app: Express = express();
-const port: number | String = process.env.PORT || 3000;
+  // // REST APi
+  // app.get("/article", async (req: Request, res: Response) => {
+  //     const articals = await Article.find({
+  //         deleted:false
+  //     })
 
+  //     res.json({
+  //         artical: articals
+  //     })
+  // });
 
-// REST APi
-app.get("/article", async (req: Request, res: Response) => {
-    const articals = await Article.find({
-        deleted:false
-    })
+  
+  // GraphQL
 
-    res.json({
-        artical: articals
-    })
-});
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({
+    app: app,
+    path: "/graphql",
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+};
+
+startSever();
